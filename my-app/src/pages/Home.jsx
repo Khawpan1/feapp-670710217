aa3e3b2005fa34b67a343f69af57815196582402
asirakorn inyoung <inyoung_a@silpakorn.edu>
2026-09-10 05:24:48 +0000

    new data and stage

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -1,8 +1,26 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+import { movies } from '../data/data';
+import MovieCard from "../components/MovieCard";
+import MovieGallery from "../components/MovieGallery";
+
 function Home() {
   return (
     <div className="p-8 text-center">
       <h1 className="text-3xl font-bold text-slate-800">🏠 หน้าแรก</h1>
       <p className="mt-2 text-slate-500">ยินดีต้อนรับสู่ MovieHub</p>
+
+      <h2 className="mt-8 text-2xl font-bold text-slate-800 mb-4">หนังแนะนำ</h2>
+
+      <div className="mx-auto mt-8 max-w-5xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        {MovieCard.slice(0, 3).map((m) => (
+          <MovieCard key={m.id} movie={m.title} />
+        ))}
+      </div>
+      <h2 className="mt-8 text-2xl font-bold text-slate-800 mb-4">ดูหนังเพิ่มเติม</h2>
+      <div className="mx-auto mt-8 max-w-md">
+        <MovieGallery />
+      </div>
     </div>
   );
 }
